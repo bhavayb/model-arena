ddb8e343d39741b0b173a60ddf2ccb8d90edac98
Bhavay Batra <btech10549.23@bitmesra.ac.in>
2025-12-21 18:51:35 +0530

    Added layout and multimodel

--- app/page.js
+++ app/page.js
@@ -1,16 +1,12 @@
 'use client';
-import Image from "next/image";
-import {Button} from "@/components/ui/button";
 import { useTheme } from "next-themes";
+import ChatInputBox from "./_components/ChatInputBox";
 
 export default function Home() {
   const {setTheme} = useTheme();
   return (
     <div>
-      <h2> Subscribe to tubeguruji</h2>
-      <Button> Subscribe </Button>
-      <Button onClick={() => setTheme('dark')}>Dark mode</Button>
-      <Button onClick={() => setTheme('light')}>Light mode</Button>
+      <ChatInputBox />
     </div>
   );
 }
